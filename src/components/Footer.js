dfc1a81446864c623602ad248fbf9cc5157b5479
Mikhail Chernyshev <newmishach@gmail.com>
2022-10-13 21:59:42 +0300

    editDeploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import React from "react";
 function Footer() {
   const year = new Date().getFullYear();
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-
+import React from "react";
 function Header({ loggedIn, email, onClick }) {
   const location = useLocation();
   return (
