084d9fb33e71e61c4bfad4d33397893e4b145a7e
Mikhail Chernyshev <newmishach@gmail.com>
2022-10-17 19:08:36 +0300

    mergeConflict

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -23,8 +23,8 @@ function ImagePopup({ card, onClose, isPopupOpened }) {
       onClick={handleClickOverlay}
     >
       <div className="popup-bigpic__content">
-        <img src={card.link} alt={card.name} className="popup-bigpic__pic" />
-        <p className="popup-bigpic__title">{card.name}</p>
+        {/* <img src={card.link} alt={card.name} className="popup-bigpic__pic" /> */}
+        {/* <p className="popup-bigpic__title">{card.name}</p> */}
         <button
           className="popup-bigpic__close popup__close"
           type="button"
