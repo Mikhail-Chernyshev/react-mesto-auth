7cc6b64c6a7b7217ceecfd937efaf133a169e095
Mikhail Chernyshev <newmishach@gmail.com>
2022-10-18 23:42:37 +0400

    fixedImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { useEffect } from "react";
+import React from 'react';
+import { useEffect } from 'react';
 function ImagePopup({ card, onClose, isPopupOpened }) {
   useEffect(() => {
     function closeByEscape(evt) {
-      console.log(card)
-      if (evt.key === "Escape") {
+      console.log(card);
+      if (evt.key === 'Escape') {
         onClose();
       }
     }
     if (isPopupOpened) {
-      document.addEventListener("keydown", closeByEscape);
-      return () => document.removeEventListener("keydown", closeByEscape);
+      document.addEventListener('keydown', closeByEscape);
+      return () => document.removeEventListener('keydown', closeByEscape);
     }
   }, [isPopupOpened]);
 
@@ -19,15 +19,23 @@ function ImagePopup({ card, onClose, isPopupOpened }) {
   }
   return (
     <div
-      className={`popup popup-bigpic ${card && "popup_opened"}`}
+      className={`popup popup-bigpic ${card && 'popup_opened'}`}
       onClick={handleClickOverlay}
     >
-      <div className="popup-bigpic__content">
-        <img src={card.link} alt={card.name} className="popup-bigpic__pic" />
-        {/* <p className="popup-bigpic__title">{card.name}</p> */}
+      <div className='popup-bigpic__content'>
+        {isPopupOpened && (
+          <div>
+            <img
+              src={card.link}
+              alt={card.name}
+              className='popup-bigpic__pic'
+            />
+            <p className='popup-bigpic__title'>{card.name}</p>
+          </div>
+        )}
         <button
-          className="popup-bigpic__close popup__close"
-          type="button"
+          className='popup-bigpic__close popup__close'
+          type='button'
           onClick={onClose}
         ></button>
       </div>
