92260f970049edb421f5edba94c0074b4aa6b511
Mikhail Chernyshev <101801297+Mikhail-Chernyshev@users.noreply.github.com>
2023-09-15 00:29:13 +0300

    fix route error

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-mesto-auth/precache-manifest.2d1c067f811a71caa11ad0ceeb970f01.js"
+  "/react-mesto-auth/precache-manifest.389a5976215e44beb7eaad68f27cb4e7.js"
 );
 
 workbox.clientsClaim();
